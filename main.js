dfb02529f442e08bdbcd08addd449f7fa13f9421
jwwhittall <junewwhittall@gmail.com>
2024-05-21 21:35:12 -0400

    blank game canvas

--- main.js
+++ main.js
@@ -1,24 +1,39 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import "./style.css";
+import Phaser from "phaser";
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const sizes = {
+    width: 800,
+    height: 550,
+};
+class GameScene extends Phaser.Scene {
+    constructor() {
+        super("scene-game");
+    }
 
-setupCounter(document.querySelector('#counter'))
+    preload() {
+    }
+
+    create() {
+    }
+
+    update() {
+    }
+
+}
+
+const config = {
+    type: Phaser.WEBGL,
+    width: sizes.width,
+    height: sizes.height,
+    canvas: gameCanvas,
+    physics: {
+        default: "arcade",
+        arcade: {
+            // gravity: { y: speedDown },
+            debug: true,
+        },
+    },
+    scene: [GameScene],
+};
+
+const game = new Phaser.Game(config);
